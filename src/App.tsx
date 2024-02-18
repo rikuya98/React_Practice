5f83cd8a42f341c864363c31e189898dd52ee348
日下陸矢 <rikuya.98@gmail.com>
2024-02-18 21:34:58 +0900

    ・Reactのプロジェクトの作成 ・react routerのインストール ・Home、記事作成コンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter ,Link, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import {ArticleCreation} from './components/ArticleCreation';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">TOPページ</Link>
+        <Link to="/article">記事作成</Link>
+      </nav>
     </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/article" element={<ArticleCreation />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
